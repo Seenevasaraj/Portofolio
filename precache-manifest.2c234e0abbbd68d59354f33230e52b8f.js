b1d0f4227a72ec58926c1399eb9a94f82e28659c
S <SSeenevasaraj@luxoft.com>
2022-05-14 10:54:40 +0530

    Updates

---
+++ precache-manifest.2c234e0abbbd68d59354f33230e52b8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "86e0541341e9ccf9e3f3",
+    "url": "/Portofolio/static/css/main.6d0a4192.chunk.css"
+  },
+  {
+    "revision": "86e0541341e9ccf9e3f3",
+    "url": "/Portofolio/static/js/main.86e05413.chunk.js"
+  },
+  {
+    "revision": "bb4f7b1c1b41c5e6f99e",
+    "url": "/Portofolio/static/js/1.bb4f7b1c.chunk.js"
+  },
+  {
+    "revision": "11e5ef72566b9f396e5a",
+    "url": "/Portofolio/static/js/runtime~main.11e5ef72.js"
+  },
+  {
+    "revision": "48c71577b7bba382052ac4c11d95883f",
+    "url": "/Portofolio/static/media/seene.48c71577.jpg"
+  },
+  {
+    "revision": "d9c121a9302660f9660cae6add6e1b27",
+    "url": "/Portofolio/static/media/bg-image.d9c121a9.jpg"
+  },
+  {
+    "revision": "55c7368fffb620a7a13e7ab481dba3bf",
+    "url": "/Portofolio/index.html"
+  }
+];
